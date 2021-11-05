770866430d77da833ac922765914de5eb6252dc8
킹강 <itkk0306@naver.com>
2021-11-05 17:32:32 +0900

    tar 변경

--- src/main/java/com/example/KingApplication.java
+++ src/main/java/com/example/KingApplication.java
@@ -1,11 +1,18 @@
 package com.example;
 
+import javafx.application.Application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KingApplication {
+public class KingApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KingApplication.class, args);
